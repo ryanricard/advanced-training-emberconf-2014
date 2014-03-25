73d5a995a22a11498e6d5f9ac3044c2f8940e3f7
Ryan Ricard <ryan.ricard@gmail.com>
2014-03-25 13:04:15 -0700

    Solution to Step 4
    Added ability to push records into store.

--- app/microdata/store.js
+++ app/microdata/store.js
@@ -1,1 +1,50 @@
-export default Ember.Object.extend()
+function buildRecord(type, data, store) {
+  var containerKey = 'model:' + type;
+
+  var factory = store.container.lookupFactory(containerKey);
+
+  var record = factory.create({
+    id: data.id,
+    $data: data
+  });
+
+  var id = data.id;
+
+  identityMapForType(type, store)[id] = record;
+
+  return record;
+}
+
+function identityMapForType(type, store) {
+  var typeIdentityMap = store.get('identityMap');
+
+  typeIdentityMap[type] = typeIdentityMap[type] || {};
+
+  return typeIdentityMap[type];
+}
+
+export default Ember.Object.extend({
+  init: function() {
+    this.set('identityMap', {});
+  },
+
+  push: function(type, data) {
+    Ember.assert("Payload is missing an id.", data.id);
+
+    var record = this.getById(type, data.id);
+
+    if (record) {
+      record.set('$data', data);
+    } else {
+      record = buildRecord(type, data, this);
+    }
+
+    return record;
+  },
+
+  getById: function(type, id) {
+    var identityMap = identityMapForType(type, this);
+
+    return identityMap[id] || null;
+  }
+})
